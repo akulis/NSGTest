3fa408cf1dee244e76a1d05bb0a75f9dea499d54
Andy Kulis <akulis@users.noreply.github.com>
2016-08-16 19:15:16 -0500

    Update alertShow.js

--- app/js/directives/alertShow.js
+++ app/js/directives/alertShow.js
@@ -6,8 +6,10 @@ four51.app.directive('alertShow', ['$animate', function($animate) {
         link: function(scope, element) {
             scope.$watch('alertShow', function(show) {
                 if (show) {
+                    $animate.enabled(true);
                     $animate.removeClass(element, 'ng-hide');
-                    $animate.addClass(element, 'ng-hide');
+                    $timeout($animate.addClass(element, 'ng-hide'), 10000); // Gives us a timeout of 10 seconds $ak
+                    $animate.enabled(false);
                 }
             });
         }
