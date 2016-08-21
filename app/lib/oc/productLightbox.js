f3cb1c7ea746a4c66584568a15e59a515f7cb175
Andy Kulis <akulis@users.noreply.github.com>
2016-08-21 13:51:41 -0500

    Update productLightbox.js

--- app/lib/oc/productLightbox.js
+++ app/lib/oc/productLightbox.js
@@ -49,7 +49,7 @@ function productlightbox() {
             '<div class="scrollLightbox">',
             '<li ng-repeat="image in LineItem.images">',
             '<a ng-click="$parent.index=$index" ng-class="{active: $index==$parent.index}">',
-            '<img ng-src="{{image.url}}" class="img-thumbnail img-responsive" />',
+            '<img ng-src="{{image.url}}" class="img-thumbnail img-responsive" alt="<option class="" value=""></option>"/>',
             '</a>',
             '</li>',
             '</div>',
